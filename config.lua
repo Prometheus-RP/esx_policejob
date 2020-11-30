e830e035f557798cc1559944ddb77bf32fe7163a
Lucas Lima <lucas39624@gmail.com>
2020-11-30 17:34:05 -0300

    -- configurando o esx_policejob

--- config.lua
+++ config.lua
@@ -5,19 +5,19 @@ Config.MarkerType                 = {Cloakrooms = 20, Armories = 21, BossActions
 Config.MarkerSize                 = {x = 1.5, y = 1.5, z = 0.5}
 Config.MarkerColor                = {r = 50, g = 50, b = 204}
 
-Config.EnablePlayerManagement     = false -- Enable if you want society managing.
-Config.EnableArmoryManagement     = false
-Config.EnableESXIdentity          = false -- Enable if you're using esx_identity.
-Config.EnableLicenses             = false -- Enable if you're using esx_license.
+Config.EnablePlayerManagement     = true -- Ativa o gerênciamento da sociedade.
+Config.EnableArmoryManagement     = true -- Ativa o o depósito de itens
+Config.EnableESXIdentity          = true -- Habilite se você estiver usando esx_identity.
+Config.EnableLicenses             = true -- Habilite se você estiver usando esx_license.
 
-Config.EnableHandcuffTimer        = true -- Enable handcuff timer? will unrestrain player after the time ends.
+Config.EnableHandcuffTimer        = true -- Ativar temporizador de algemas? irá liberar o jogador após o término do tempo.
 Config.HandcuffTimer              = 10 * 60000 -- 10 minutes.
 
-Config.EnableJobBlip              = false -- Enable blips for cops on duty, requires esx_society.
-Config.EnableCustomPeds           = false -- Enable custom peds in cloak room? See Config.CustomPeds below to customize peds.
+Config.EnableJobBlip              = true -- Habilitar blips para policiais de plantão, requer esx_society.
+Config.EnableCustomPeds           = false -- Ativa peds personalizados no cloak room? Veja Config.CustomPeds abaixo para personalizar peds.
 
-Config.EnableESXService           = false -- Enable esx service?
-Config.MaxInService               = -1 -- How much people can be in service at once?
+Config.EnableESXService           = true -- Enable esx_service?
+Config.MaxInService               = 8 -- Quantas pessoas podem estar em serviço ao mesmo tempo?
 
 Config.Locale                     = 'br'
 
